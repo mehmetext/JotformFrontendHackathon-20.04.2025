2a6509b95594cf289d142243299a17c34e4ce47b
Mehmet <mehmetext68@gmail.com>
2025-04-19 09:42:38 +0300

    feat: customize container class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Jotform Frontend Hackathon</h1>
+    <div className="container">
+      <div className="h-screen w-full flex items-center justify-center">
+        <h1 className="text-4xl">Jotform Frontend Hackathon</h1>
+      </div>
     </div>
   );
 }
